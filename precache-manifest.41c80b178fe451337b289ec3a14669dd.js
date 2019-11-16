6cab2fcdf01ecdccdb9dece36ccfae5b6589b243
ubogov <sergeiubogov@mail.ru>
2019-11-16 13:22:07 +0300

    Updates

---
+++ precache-manifest.41c80b178fe451337b289ec3a14669dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "901111698b1ca20dc45967a7ec1dfd86",
+    "url": "/tinkoff-invest/index.html"
+  },
+  {
+    "revision": "23297b9b4b3db2644175",
+    "url": "/tinkoff-invest/static/css/main.f4fbe76f.chunk.css"
+  },
+  {
+    "revision": "651cd564a8a4375ede0a",
+    "url": "/tinkoff-invest/static/js/2.4a8f2c63.chunk.js"
+  },
+  {
+    "revision": "23297b9b4b3db2644175",
+    "url": "/tinkoff-invest/static/js/main.aaacda39.chunk.js"
+  },
+  {
+    "revision": "2b11b72d2012396a5923",
+    "url": "/tinkoff-invest/static/js/runtime-main.55efd48e.js"
+  }
+]);
